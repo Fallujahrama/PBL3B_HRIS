7481d53eb1456d901589a1e7c51d9e16ab77f85d
yusra_ys <yusrayusuf811@gmail.com>
2025-11-27 14:51:04 +0700

    Fitur employee RestAPI (Yusra dan Aldo)

--- API/routes/api.php
+++ API/routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\EmployeeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\LetterFormatController;
 use App\Http\Controllers\Api\LetterController;
 
+
 Route::apiResource('letter-formats', LetterFormatController::class);
 Route::apiResource('letters', LetterController::class);
+
+Route::apiResource('employees', EmployeeController::class);
